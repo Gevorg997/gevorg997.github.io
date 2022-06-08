ce429751dcb4e4f0d207ced25224b1f89ee42898
gevorg-babayan <99825517+gevorg-babayan@users.noreply.github.com>
2022-06-08 15:42:25 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+function myDate(){
+    var x = document.getElementById("date1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    
+  } else {
+    x.style.display = "none";
+    
+}
+}
+
+function myPlace(){
+  var x = document.getElementById("place1");
+if (x.style.display === "none") {
+  x.style.display = "block";
+  
+} else {
+  x.style.display = "none";
+  
+}
+}
+
+function myEdu(){
+  var x = document.getElementById("edu1");
+if (x.style.display === "none") {
+  x.style.display = "block";
+  
+} else {
+  x.style.display = "none";
+  
+}
+}
+
+function myExp(){
+  var x = document.getElementById("exp1");
+if (x.style.display === "none") {
+  x.style.display = "block";
+  
+} else {
+  x.style.display = "none";
+  
+}
+}
+
+
+
+
